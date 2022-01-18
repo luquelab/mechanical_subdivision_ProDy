5146d8c96309b290be2b64b56f8551ac59366913
colintravisbrown <54756825+colintravisbrown@users.noreply.github.com>
2022-01-17 23:04:02 -0800

    Changes how score plotting is done.

--- src/score.py
+++ src/score.py
@@ -14,3 +14,40 @@ def cluster_types(labels):
     var = np.std(counts)
     ntypes = np.unique(counts).shape[0]
     return var, ntypes
+
+def plotScores(pdb, n_range):
+    import matplotlib.pyplot as plt
+    scores = []
+    vars = []
+    ntypes = []
+    for i in range(len(n_range)):
+        nc = n_range[i]
+        results = np.load('../results/subdivisions/' + pdb + '/' + pdb + '_' + nc + '_results')
+        score = results['score']
+        ntype = results['ntypes']
+        var = results['var']
+        scores.append(score)
+        vars.append(var)
+        ntypes.append(ntype)
+    scores = np.array(scores)
+    vars = np.array(vars)
+    ntypes = np.array(ntypes)
+    print('Plotting')
+    fig, ax = plt.subplots(3, 1, figsize=(16, 10), sharex=True)
+    ax[0].scatter(n_range, scores, marker='D', label=pdb)
+    ax[0].plot(n_range, scores)
+    ax[1].plot(n_range, ntypes)
+    ax[1].scatter(n_range, ntypes)
+    ax[1].set_ylabel('# of unique clusters')
+    ax[2].plot(n_range, vars)
+    ax[2].scatter(n_range, vars)
+    ax[2].set_ylabel('Variance In Cluster Size')
+    ax[0].axvline(x=n_range[np.argmax(scores)], label='Best Score', color='black')
+    nc = str(n_range[np.argmax(scores)])
+    ax[0].set_xticks(n_range)
+    ax[2].set_xlabel('n_clusters')
+    ax[0].set_ylabel('Silhouette Score')
+    ax[0].legend()
+    fig.tight_layout()
+    print(pdb + '_' + nc + '_domains.png')
+    plt.show()
--- src/subdivide_model.py
+++ src/subdivide_model.py
@@ -64,7 +64,7 @@ def subdivide_model(pdb, cluster_start, cluster_stop, cluster_step):
 
             print('Saving Results')
             nc = str(n_range[n])
-            np.savez('../results/subdivisions/' + pdb + '/' + pdb + '_' + nc + '_results', labels=label, score=testScore)
+            np.savez('../results/subdivisions/' + pdb + '/' + pdb + '_' + nc + '_results', labels=label, score=testScore, var=var, ntypes=ntypes,n=n)
             labels.append(label)
 
         best = np.argpartition(scores_km, -5)[-5:]  # indices of 4 best scores
@@ -102,11 +102,11 @@ def subdivide_model(pdb, cluster_start, cluster_stop, cluster_step):
     fig, ax = plt.subplots(3, 1, figsize=(16, 10), sharex=True)
     ax[0].scatter(n_range, scores, marker='D', label=pdb)
     ax[0].plot(n_range, scores)
-    ax[1].plot(n_range, nt)
-    ax[1].scatter(n_range, nt)
+    ax[1].plot(n_range, ntypes)
+    ax[1].scatter(n_range, ntypes)
     ax[1].set_ylabel('# of unique clusters')
-    ax[2].plot(n_range, v)
-    ax[2].scatter(n_range, v)
+    ax[2].plot(n_range, var)
+    ax[2].scatter(n_range, var)
     ax[2].set_ylabel('Variance In Cluster Size')
     ax[0].axvline(x=n_range[np.argmax(scores)], label='Best Score', color='black')
     nc = str(n_range[np.argmax(scores)])
